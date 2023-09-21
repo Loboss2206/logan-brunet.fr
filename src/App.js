bff253f21e761cdcf0ce9356a420749864b67b54
Logan <logan@pop-os.localdomain>
2023-09-21 12:39:30 +0200

    react and tailwind base code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './tailwind.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-green-500'>PORTFOLIO</p>
+    </div>
+  );
+}
+
+export default App;
