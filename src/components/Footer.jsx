76843a63a54faf4f4f319b299f797d31beb8038e
Logan Brunet <98391380+Loboss2206@users.noreply.github.com>
2024-05-10 19:14:09 +0200

    style: improve style of the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Menu = () => {
-    const height = "50";
-    const margin = "4";
-
     return (
         <div className="w-full h-20 bg-gray-800 bg-opacity-95 text-white flex items-center justify-center z-50 mt-16">
-            <a href="#">
-                <p className="text-green-400 text-2xl font-bold mt-1.5 mx-16">{`Made by Logan Brunet\u00A0\u00A0🪴\u00A0\u00A0with React and Tailwind`}</p>
-            </a>
+            <motion.p className="text-green-400 text-2xl font-bold">
+                {"Made by Logan Brunet\u00A0\u00A0🪴\u00A0\u00A0with "}
+                <a href="https://fr.legacy.reactjs.org/" target="_blank"><span className="hover:text-green-700 hover:cursor-pointer duration-200"><i>React</i></span></a> {"and "}
+                <a href="https://tailwindcss.com/" target="_blank"><span className="hover:text-green-700 hover:cursor-pointer duration-200"><i>Tailwind</i></span></a>
+            </motion.p>
         </div >
     );
 };
