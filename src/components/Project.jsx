1edb07364ce63bf9e6e65d2564da8c478b7eaf9b
Logan Brunet <98391380+Loboss2206@users.noreply.github.com>
2024-05-20 12:21:26 +0200

    style: responsive the projects section of the portfolio

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 
 const Project = ({ imageSrc, date, title, description, link }) => {
     return (
-        <a href={(link === undefined) ? '#' : link}><div className="relative overflow-hidden bg-green-800 bg-opacity-90 shadow-lg shadow-white/50 rounded-md transition-transform transform hover:scale-105 h-108">
+        <a href={(link === undefined) ? '#' : link}><div className="relative overflow-hidden bg-gray-800/80 shadow-lg shadow-white/50 rounded-md transition-transform transform hover:scale-105 h-108">
             <div className="absolute inset-0 rounded-md"></div>
-            <img src={imageSrc} alt="Project3" className="max-h-48 w-auto rounded-md mb-4 mx-auto mt-2" />
+            <img src={imageSrc} alt="Project3" className="h-44 w-auto rounded-md mb-4 mx-auto mt-2" />
             <div className="p-4 relative z-10">
                 <p className="text-white">Date: {date}</p>
                 <h3 className="text-white text-2xl font-bold mb-2">{title}</h3>
-                <p className="text-white">{description}</p>
+                <p className="text-white limit7lines">{description}</p>
             </div>
         </div></a>
     );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,7 +28,7 @@ const Projects = () => {
                 {isTitleAnimationComplete && (
                     <div
                         ref={projectsRef}
-                        className="opacity-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 sm:px-0 mt-4"
+                        className="opacity-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 sm:px-0 p-4 mt-4"
                     >
                         {projects.map(({ id, imageSrc, date, title, description, link }) => (
                             <Project
