bbc6c10bdee780133c053df0a28cae6c0e0f997e
Logan Brunet <98391380+Loboss2206@users.noreply.github.com>
2024-07-08 14:22:41 +0200

    fix: fix the footer on phone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,19 @@ const Menu = () => {
   const size = "25px";
   return (
     <div className="w-full h-24 sm:h-20 bg-gray-800 bg-opacity-95 text-white flex items-center justify-center z-50 mt-16">
-      <motion.p className="flex text-green-400 text-xl sm:text-2xl font-bold">
+      <motion.p className="block sm:flex text-green-400 text-xl sm:text-2xl font-bold">
         <motion.span className="flex">
           {"Made by Logan Brunet\u00A0\u00A0"}
           <img src={cactus} width={size} height={size}></img>
           {"\u00A0\u00A0with\u00A0"}
         </motion.span>
-        <motion.span className="block sm:inline text-center">
+        <motion.span className="flex justify-center">
           <motion.a href="https://fr.legacy.reactjs.org/" target="_blank">
             <span className="hover:text-green-700 hover:cursor-pointer duration-200">
               <i>React</i>
             </span>
-          </motion.a>{" "}
-          {"and "}
+          </motion.a>
+          {"\u00A0and\u00A0"}
           <motion.a href="https://tailwindcss.com/" target="_blank">
             <span className="hover:text-green-700 hover:cursor-pointer duration-200">
               <i>Tailwind</i>
