8a0707ba3f1e973476d92b7b03d01ffb90eab3e4
Logan Brunet <98391380+Loboss2206@users.noreply.github.com>
2024-07-09 00:18:37 +0200

    fix: remove the object cover on the skill which was the source of a visual bug

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,11 +3,15 @@ import React from "react";
 const Skill = ({ imageSrc, title, link }) => {
   return (
     <div className="flex flex-col items-center justify-center m-5">
-      <a target="_blank" href={link === undefined ? "#" : link}>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={link === undefined ? "#" : link}
+      >
         <img
           src={imageSrc}
           alt={title}
-          className="w-32 h-32 object-cover duration-500 cursor-pointer duration-300 hover:scale-110"
+          className="w-20 h-20 duration-500 cursor-pointer duration-300 hover:scale-110"
         />
       </a>
       <h3 className="text-white text-lg font-semibold mt-2 text-center">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,89 +2,127 @@ import React, { useState, useEffect, useRef } from "react";
 import CustomTitle from "./CustomTitle";
 import Skill from "./Skill";
 
-import langagesMock from "../datas/langages"
-import osMock from "../datas/os"
-import toolsMock from "../datas/tools"
+import langagesMock from "../datas/langages";
+import osMock from "../datas/os";
+import toolsMock from "../datas/tools";
 
 const Skills = () => {
-    const [isFirstTitleAnimationComplete, setIsFirstTitleAnimationComplete] = useState(false);
-    const [isSecondTitleAnimationComplete, setIsSecondTitleAnimationComplete] = useState(false);
-    const [isThirdTitleAnimationComplete, setIsThirdTitleAnimationComplete] = useState(false);
-    const [isFourthTitleAnimationComplete, setIsFourthTitleAnimationComplete] = useState(false);
+  const [isFirstTitleAnimationComplete, setIsFirstTitleAnimationComplete] =
+    useState(false);
+  const [isSecondTitleAnimationComplete, setIsSecondTitleAnimationComplete] =
+    useState(false);
+  const [isThirdTitleAnimationComplete, setIsThirdTitleAnimationComplete] =
+    useState(false);
+  const [isFourthTitleAnimationComplete, setIsFourthTitleAnimationComplete] =
+    useState(false);
 
-    const langages = langagesMock;
+  const langages = langagesMock;
 
-    const os = osMock;
+  const os = osMock;
 
-    const tools = toolsMock;
+  const tools = toolsMock;
 
-    const langagesRef = useRef(null);
-    const osRef = useRef(null);
-    const toolsRef = useRef(null);
+  const langagesRef = useRef(null);
+  const osRef = useRef(null);
+  const toolsRef = useRef(null);
 
-    useEffect(() => {
-        if (isSecondTitleAnimationComplete) {
-            langagesRef.current.classList.add('fade-in');
-        }
-    }, [isSecondTitleAnimationComplete]);
+  useEffect(() => {
+    if (isSecondTitleAnimationComplete) {
+      langagesRef.current.classList.add("fade-in");
+    }
+  }, [isSecondTitleAnimationComplete]);
 
-    useEffect(() => {
-        if (isThirdTitleAnimationComplete) {
-            osRef.current.classList.add('fade-in');
-        }
-    }, [isThirdTitleAnimationComplete]);
+  useEffect(() => {
+    if (isThirdTitleAnimationComplete) {
+      osRef.current.classList.add("fade-in");
+    }
+  }, [isThirdTitleAnimationComplete]);
 
-    useEffect(() => {
-        if (isFourthTitleAnimationComplete) {
-            toolsRef.current.classList.add('fade-in');
-        }
-    }, [isFourthTitleAnimationComplete]);
+  useEffect(() => {
+    if (isFourthTitleAnimationComplete) {
+      toolsRef.current.classList.add("fade-in");
+    }
+  }, [isFourthTitleAnimationComplete]);
 
-    return (
-        <div
-            name="Skills"
-            className="flex items-center min-h-screen w-full flex-col bg-gradient-to-r from-background-color to-container-bg px-5 pt-20"
-        >
-            <div className="flex flex-col w-full 2xl:w-2/3">
-                <CustomTitle title="cd ~/Skills" margin="12" onAnimationEnd={() => setIsFirstTitleAnimationComplete(true)} />
-                {isFirstTitleAnimationComplete && (
-                    <>
-                        <CustomTitle title="ls Langages&Frameworks" margin="0" onAnimationEnd={() => setIsSecondTitleAnimationComplete(true)} />
-                        {isSecondTitleAnimationComplete && (
-                            <div ref={langagesRef} className="opacity-0 bg-gray-900/90 dark:bg-gray-800/80 shadow-lg shadow-gray-900/80 dark:shadow-white/50 rounded-lg grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 gap-8 sm:px-0 p-4 mt-4">
-                                {langages.map(({ id, imageSrc, title, link }) => (
-                                    <Skill key={id} imageSrc={imageSrc} link={link} title={title} />
-                                ))}
-                            </div>
-                        )}
-                    </>
-                )}
-                {isSecondTitleAnimationComplete && (
-                    <>
-                        <CustomTitle title="ls OperatingSystems" margin="12" onAnimationEnd={() => setIsThirdTitleAnimationComplete(true)} />
-                        {isThirdTitleAnimationComplete && (
-                            <div ref={osRef} className="opacity-0 bg-gray-900/90 dark:bg-gray-800/80 shadow-lg shadow-gray-900/80 dark:shadow-white/50 rounded-lg grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 gap-8 sm:px-0 p-4 mt-4">
-                                {os.map(({ id, imageSrc, title, link }) => (
-                                    <Skill key={id} imageSrc={imageSrc} link={link} title={title} />
-                                ))}
-                            </div>
-                        )}
-                    </>
-                )}
-                {isThirdTitleAnimationComplete && (
-                    <>
-                        <CustomTitle title="ls Tools" margin="12" onAnimationEnd={() => setIsFourthTitleAnimationComplete(true)} />
-                        {isFourthTitleAnimationComplete && (
-                            <div ref={toolsRef} className="opacity-0 bg-gray-900/90 dark:bg-gray-800/80 shadow-lg shadow-gray-900/80 dark:shadow-white/50 rounded-lg grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 gap-8 sm:px-0 p-4 mt-4">
-                                {tools.map(({ id, imageSrc, title, link }) => (
-                                    <Skill key={id} imageSrc={imageSrc} link={link} title={title} />
-                                ))}
-                            </div>
-                        )}
-                    </>
-                )}
-            </div>
-        </div>
-    );
+  const skillBoxesStyle =
+    "opacity-0 bg-gray-900/90 dark:bg-gray-800/80 shadow-lg shadow-gray-900/80 dark:shadow-white/50 rounded-lg grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 p-5 mt-4";
+
+  return (
+    <div
+      name="Skills"
+      className="flex items-center min-h-screen w-full flex-col bg-gradient-to-r from-background-color to-container-bg px-5 pt-20"
+    >
+      <div className="flex flex-col w-full 2xl:w-2/3">
+        <CustomTitle
+          title="cd ~/Skills"
+          margin="12"
+          onAnimationEnd={() => setIsFirstTitleAnimationComplete(true)}
+        />
+        {isFirstTitleAnimationComplete && (
+          <>
+            <CustomTitle
+              title="ls Langages&Frameworks"
+              margin="0"
+              onAnimationEnd={() => setIsSecondTitleAnimationComplete(true)}
+            />
+            {isSecondTitleAnimationComplete && (
+              <div ref={langagesRef} className={skillBoxesStyle}>
+                {langages.map(({ id, imageSrc, title, link }) => (
+                  <Skill
+                    key={id}
+                    imageSrc={imageSrc}
+                    link={link}
+                    title={title}
+                  />
+                ))}
+              </div>
+            )}
+          </>
+        )}
+        {isSecondTitleAnimationComplete && (
+          <>
+            <CustomTitle
+              title="ls OperatingSystems"
+              margin="12"
+              onAnimationEnd={() => setIsThirdTitleAnimationComplete(true)}
+            />
+            {isThirdTitleAnimationComplete && (
+              <div ref={osRef} className={skillBoxesStyle}>
+                {os.map(({ id, imageSrc, title, link }) => (
+                  <Skill
+                    key={id}
+                    imageSrc={imageSrc}
+                    link={link}
+                    title={title}
+                  />
+                ))}
+              </div>
+            )}
+          </>
+        )}
+        {isThirdTitleAnimationComplete && (
+          <>
+            <CustomTitle
+              title="ls Tools"
+              margin="12"
+              onAnimationEnd={() => setIsFourthTitleAnimationComplete(true)}
+            />
+            {isFourthTitleAnimationComplete && (
+              <div ref={toolsRef} className={skillBoxesStyle}>
+                {tools.map(({ id, imageSrc, title, link }) => (
+                  <Skill
+                    key={id}
+                    imageSrc={imageSrc}
+                    link={link}
+                    title={title}
+                  />
+                ))}
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 export default Skills;
