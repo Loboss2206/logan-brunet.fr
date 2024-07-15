507f5f00cfbda706011d5cad14eadfc120aff170
Logan Brunet <98391380+Loboss2206@users.noreply.github.com>
2024-07-15 15:59:34 +0200

    fix: fix the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
